00b1037bb5858e82ecc77bb03cf6fd26e8d862f2
Matthew Madden <matthewmadden@Matthews-MBP-4.hsd1.pa.comcast.net>
2021-04-13 21:28:16 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './CalculatorImage.jpg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Annuity Calculator
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Calculator
         </a>
       </header>
     </div>
